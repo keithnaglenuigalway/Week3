012c3a6c6fd095617c72ddc61a3518303d7ec5de
keithnaglenuigalway <86240520+keithnaglenuigalway@users.noreply.github.com>
2021-11-25 21:44:26 +0000

    Create q2_bar_charts.c

---
+++ q2_bar_charts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	// read and store five numbers (between 1 and 30)
+	
+	// For each number read, print a line containing containing that number of asterisks
+	
+	return 0;
+}
